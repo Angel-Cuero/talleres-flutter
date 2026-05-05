0bf10952affa56b5ed3d7f329bdf3eb952988657
Angel-Cuero <angel.cuero01@uceva.edu.co>
2026-05-04 23:43:37 -0500

    Configuración de Firebase y main.dart inicializado (Versión 1.0.0+1).

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:electiva_2026/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'themes/app_theme.dart'; // Importar el tema
+import 'package:firebase_core/firebase_core.dart'; // Importa esto
+import 'firebase_options.dart'; // Importa el archivo generado
 
-void main() {
+void main() async {
+  // 2. Esta línea es obligatoria cuando inicializas algo antes del runApp
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 3. Inicializamos Firebase usando las opciones generadas
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
